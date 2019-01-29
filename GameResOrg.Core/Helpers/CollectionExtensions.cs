ee32766613f522b07ad1ffb25a5d0d88c2944c2a
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-29 13:04:24 +0100

    Added misc/minor helpers project files

---
+++ GameResOrg.Core/Helpers/CollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace System.Linq
+{
+	public static class CollectionExtensions
+	{
+		/*
+		/// <summary>
+		/// Creates a new list with elements from s and specified capacity.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="s"></param>
+		/// <param name="count"></param>
+		/// <returns></returns>
+		[NotNull]
+		public static List<T> ToList<T>([NotNull] this IEnumerable<T> s, int count)
+		{
+			if (count < 0) throw new ArgumentOutOfRangeException("count");
+			var list = new List<T>(count);
+			list.AddRange(s);
+			return list;
+		}*/
+
+		/// <summary>
+		/// if list is empty, returns null
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		[CanBeNull]
+		public static List<T> ToListOrNull<T>([NotNull] this IEnumerable<T> s)
+		{
+			var list = s.ToList();
+			return list.Count > 0 ? list : null;
+		}
+	}
+}
---
+++ GameResOrg.Core/ImageSourceExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GameResOrg.Controls.Api
+{
+	public sealed class ImageSourceExtension : IMarkupExtension<ImageSource>
+	{
+		public string Path { get; set; }
+
+		public ImageSource ProvideValue(IServiceProvider serviceProvider)
+		{
+			return FileImageSource.FromResource(Path, Assembly.GetExecutingAssembly());
+		}
+
+		object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+		{
+			return ProvideValue(serviceProvider);
+		}
+	}
+}
