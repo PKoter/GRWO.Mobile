d373a5d9a395d0595704dd88d63c07f5ca6a86c0
Brat Wiekszy <bratwiekszy@gmail.com>
2019-01-29 13:05:48 +0100

    Added project files and app entry point.

---
+++ GameResOrg/GameResOrg/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using GameResOrg.Glue;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace GameResOrg
+{
+	public partial class App : Application
+	{
+		private Mold _mold;
+
+		public App()
+		{
+			InitializeComponent();
+			_mold = new Mold();
+			MainPage = new LoginPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
